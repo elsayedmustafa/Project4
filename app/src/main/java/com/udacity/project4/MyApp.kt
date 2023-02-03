5a716a55c2d71a6bbddedd8eecafab35b24d6e2b
MustafaAlSayed <m.alsayed@cashin.sa>
2023-02-03 16:56:08 +0200

    Login and welcome

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -1,11 +1,14 @@
 package com.udacity.project4
 
 import android.app.Application
+import com.udacity.project4.base.BaseViewModel
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.loginreminderapp.RemindersLoginViewModel
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.locationreminders.welcomreminderapp.WelcomeRemindersViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -25,6 +28,12 @@ class MyApp : Application() {
                 RemindersListViewModel(
                     get(),
                     get() as ReminderDataSource
+                )
+                RemindersLoginViewModel(
+                    get()
+                )
+                WelcomeRemindersViewModel(
+                    get()
                 )
             }
             //Declare singleton definitions to be later injected using by inject()
--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -7,24 +7,31 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRemindersBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
-
+//        setContentView(R.layout.activity_reminders)
+        binding= DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_reminders
+        )
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                (binding.navHostFragment as NavHostFragment).navController.popBackStack()
                 return true
             }
         }
